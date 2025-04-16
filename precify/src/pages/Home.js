5595c89b06fb746b4fce483e5220a260b4f7c15a
adrian <alopezbanobre@gmail.com>
2025-04-16 19:42:40 -0400

    Creación de archivos principales.

---
+++ precify/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/home.css';
+
+const Home = () => {
+  return (
+    <div className="parent">
+      <div className="div1">1</div>
+      <div className="div2">2</div>
+    </div>
+  );
+};
+
+export default Home;
